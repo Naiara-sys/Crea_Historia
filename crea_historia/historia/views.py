106b82536efbe6b83da54f94854ba05cf4e73cc5
Naiara Antolin <naiaraantolinperez@gmail.com>
2025-12-16 13:19:21 +0100

    Paso 6: Vistas de la historia definidas y código limpio

--- crea_historia/historia/views.py
+++ crea_historia/historia/views.py
@@ -1,3 +1,42 @@
+# historia/views.py
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+    contexto = {
+        'titulo': 'La aventura comienza',
+        'texto': 'Te despiertas en un mundo desconocido. Frente a ti hay dos caminos.',
+        'imagen': 'https://via.placeholder.com/400'
+    }
+    return render(request, 'historia/pagina.html', contexto)
+
+def bosque(request):
+    contexto = {
+        'titulo': 'El bosque encantado',
+        'texto': 'El bosque es oscuro, pero sientes una energía extraña a tu alrededor.',
+        'imagen': 'https://via.placeholder.com/400'
+    }
+    return render(request, 'historia/pagina.html', contexto)
+
+def cueva (request):
+    contexto = {
+        'titulo': 'La cueva misteriosa',
+        'texto': 'Dentro de la cueva escuchas ruidos inquietantes.',
+        'imagen': 'https://via.placeholder.com/400'
+    }
+    return render(request, 'historia/pagina.html', contexto)
+
+def final_bueno (request):
+    contexto = {
+        'titulo': 'Final feliz',
+        'texto': 'Has tomado buenas decisiones y consigues salir con vida.',
+        'imagen': 'https://via.placeholder.com/400'
+    }
+    return render (request, 'historia/final.html', contexto)
+
+def final_malo(request):
+    contexto = {
+        'titulo': 'Final triste',
+        'texto': 'Tus decisiones te han llevado a un final desafortunado.',
+        'imagen': 'https://via.placeholder.com/400'
+    }
+    return render(request, 'historia/final.html', contexto)
